93ddfba7222b898922898d9061155724f9ee1c46
David Bernal <davidbernal@Davids-MacBook-Air-2.local>
2023-08-31 12:47:57 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, jsonify, request
+import sqlite3
+import os
+
+app = Flask(__name__)
+
+# Database setup
+def init_db():
+    os.makedirs('database', exist_ok=True)
+    conn = sqlite3.connect('database/barnabas.db')
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS documents (id INTEGER PRIMARY KEY, content TEXT)')
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/documents', methods=['GET'])
+def get_documents():
+    try:
+        os.makedirs('database', exist_ok=True)
+        conn = sqlite3.connect('database/barnabas.db')
+        c = conn.cursor()
+        c.execute('SELECT * FROM documents')
+        documents = [{"id": id, "content": content} for id, content in c.fetchall()]
+        conn.close()
+        return jsonify(documents)
+    except Exception as e:
+        return str(e), 500
+
+@app.route('/documents', methods=['POST'])
+def create_document():
+
+  conn = sqlite3.connect('database/barnabas.db')
+  c = conn.cursor()
+
+  # Insert document into database
+  c.execute('INSERT INTO documents (content) VALUES (?)', (request.json['content'],))
+
+  # Get id of inserted row
+  id = c.lastrowid
+
+  conn.commit()
+  conn.close()
+
+  return jsonify({'id': id})
+
+@app.route('/documents/<int:id>', methods=['GET'])
+def get_document(id):
+
+  conn = sqlite3.connect('database/barnabas.db')
+  c = conn.cursor()
+
+  c.execute('SELECT * FROM documents WHERE id=?', (id,))
+  document = c.fetchone()
+
+  conn.close()
+
+  if document:
+    return jsonify(document)
+  else:
+    return 'Document not found', 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,219 @@
+// Initialize Quill editor
+var quill = new Quill('#editor-container', {
+    theme: 'snow'
+});
+
+// Get the elements
+var newDocumentButton = document.getElementById('new-document-button');
+var documentList = document.getElementById('document-list');
+var contextTextarea = document.getElementById('context-textarea');
+var brainstormButton = document.getElementById('brainstorm-button');
+var interactionButtons = document.getElementById('interaction-buttons');
+var barnabasInputText = document.getElementById('barnabas-input-text');
+var sendButton = document.getElementById('send-button');
+var responseText = document.getElementById('response-text');
+
+// Global variable to keep track of the current document index
+var currentDocumentIndex = null;
+
+// Add event listener to new document button
+newDocumentButton.addEventListener('click', function() {
+    // Create a new document
+    createNewDocument();
+});
+
+// Add event listener to brainstorm button
+brainstormButton.addEventListener('click', function() {
+    // Get the context text
+    var context = contextTextarea.value;
+
+    // Get the brainstorm response
+    var response = brainstorm(context);
+
+    // Append the response to the Quill editor
+    quill.clipboard.dangerouslyPasteHTML(response);
+});
+
+// Add event listener to send button
+sendButton.addEventListener('click', function() {
+    // Get the text from the barnabas input text
+    var text = barnabasInputText.value;
+
+    // Clear the barnabas input text
+    barnabasInputText.value = '';
+
+    // Send the text to Barnabas and get the response
+    var response = askBarnabas(text);
+
+    // Display the question and response in the response text
+    responseText.innerHTML = '<small>' + text + '</small><br>' + response;
+});
+
+// Add event listener to interaction buttons
+interactionButtons.addEventListener('click', function(event) {
+    // Get the clicked button
+    var button = event.target;
+
+    // Get the selected text
+    var selection = quill.getSelection();
+    var selectedText = selection ? quill.getText(selection.index, selection.length) : '';
+
+    // Send the selected text to Barnabas and get the response
+    var response = interact(button.id, selectedText);
+
+    // Display the question and response in the response text
+    responseText.innerHTML = '<small>' + selectedText + '</small><br>' + response;
+});
+
+// Function to create a new document
+function createNewDocument() {
+    // Send a POST request to the backend API to create a new document
+    fetch('/documents', {
+        method: 'POST',
+    })
+        .then(response => {
+            let newDocId = response.id; // Get the id from response
+
+            // Save newDocId to use later
+        })
+        .then(data => {
+            // Handle the response from the backend API
+            // For example, add the new document to the document list
+        })
+        .catch(error => {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        });
+}
+
+
+// Function to load a document
+function loadDocument(docId) {
+
+  // Build URL with doc id
+  const url = `/documents/${docId}`;
+
+  // Make GET request to load document
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      // Get document contents from response
+      const docContents = data.contents;
+
+      // Set editor content
+      quill.setContents(docContents);
+
+      // Update UI with loaded document
+
+    })
+    .catch(error => {
+      console.error('Error loading document:', error);
+    });
+
+}
+
+
+// Function to brainstorm
+function brainstorm(context) {
+    // Send a POST request to the backend API to get the brainstorm response
+    fetch('/brainstorm', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ context: context }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend API
+            // For example, append the response to the Quill editor
+            quill.clipboard.dangerouslyPasteHTML(data.response);
+        })
+        .catch(error => {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        });
+}
+
+
+// Function to ask Barnabas a question and get a response
+function askBarnabas(text) {
+    // Send a POST request to the backend API to get the response from Barnabas
+    fetch('/ask-barnabas', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: text }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend API
+            // For example, display the question and response in the response text
+            responseText.innerHTML = '<small>' + text + '</small><br>' + data.response;
+        })
+        .catch(error => {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        });
+}
+
+
+// Function to interact with selected text and get a response
+function interact(action, selectedText) {
+    // For now, return a fixed response
+    return 'This is a response for the action: ' + action + ' and the selected text: ' + selectedText;
+}
+
+// Load the saved documents from local storage
+loadDocument();
+
+// Function to load the saved documents from backend API
+function loadDocument(documentId) {
+    // Send a GET request to the backend API to get the content of the document
+    fetch('/documents/' + documentId)
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend API
+            // For example, load the document content into the Quill editor
+            quill.setContents(data.content);
+        })
+        .catch(error => {
+            // Handle any errors that occur during the request
+            console.error('Error:', error);
+        });
+}
+
+
+// Add a text change event listener to the Quill editor
+quill.on('text-change', function() {
+    // If there is no current document index, return
+    if (currentDocumentIndex === null) return;
+
+    // Get the content of the Quill editor
+    var content = quill.getContents();
+
+    // Get the saved documents from local storage
+    var savedDocuments = JSON.parse(localStorage.getItem('documents')) || [];
+
+    // Save the current document to the saved documents
+    savedDocuments[currentDocumentIndex] = content;
+
+    // Save the saved documents to local storage
+    localStorage.setItem('documents', JSON.stringify(savedDocuments));
+});
+
+// Add a blur event listener to the document list
+documentList.addEventListener('blur', function(event) {
+    // If the target is not a document item, return
+    if (event.target.className !== 'document-item') return;
+
+    // Get the document index
+    var index = Array.from(documentList.children).indexOf(event.target);
+
+    // Update the document name in local storage
+    var documentNames = JSON.parse(localStorage.getItem('document-names')) || [];
+    documentNames[index] = event.target.textContent;
+    localStorage.setItem('document-names', JSON.stringify(documentNames));
+}, true);
